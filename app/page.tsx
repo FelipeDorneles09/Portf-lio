3a703bfdb17aa1eb9e622bb75946ba7b4a04e301
Felipe <fdorneles63@gmail.com>
2025-04-09 15:28:27 -0300

    kdawdwak

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,35 @@
-import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import Grid from "@/components/ui/Grid";
+"use client";
+
+import dynamic from "next/dynamic";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
+
+// Importações dinâmicas
+// Importações dinâmicas com tipagem correta
+const FloatingNav = dynamic(() =>
+  import("@/components/ui/FloatingNav").then(mod => mod.FloatingNav), // <- acessando o export nomeado
+  { ssr: false }
+);
+const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
+const Grid = dynamic(() => import("@/components/ui/Grid"), { ssr: false });
+const RecentProjects = dynamic(() => import("@/components/RecentProjects"), { ssr: false });
+const Clients = dynamic(() => import("@/components/Clients"), { ssr: false });
+const Experience = dynamic(() => import("@/components/Experience"), { ssr: false });
+const Approach = dynamic(() => import("@/components/Approach"), { ssr: false });
+const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center overflow-clip flex-col mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
-      <FloatingNav navItems={navItems}/>
-      <Hero/>
-      <Grid/>
-      <RecentProjects/>
-      <Clients />
-      <Experience />
-      <Approach />
-      <Footer />
-    </div>
-  </main>
-  )
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
 }
